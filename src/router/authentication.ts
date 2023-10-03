85397d3e8fe0bf5f89ef20ce24f1f82ae3d9de92
drex007 <vykeoj@gmail.com>
2023-10-03 17:07:37 +0000

    edited get users endpoints

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -7,7 +7,7 @@ import { isAuthenticated } from '../middlewares/jwts';
 export default(router: express.Router) =>{
 router.post('/auth/register', register);
 router.post('/auth/login', login);
-router.get('/auth/get-users',isAuthenticated,getUsers);
+router.get('/auth/get-users',getUsers);
 router.get('', allRoutes);
 router.post('/auth/balance', getUserBalance)
 
